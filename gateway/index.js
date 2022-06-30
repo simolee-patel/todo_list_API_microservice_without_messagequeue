44ed5ca34826dffd6a3bbe95cf337c3fc27b2ba4
Simolee Patel <patelsimolee12@gmail.com>
2022-06-30 15:48:56 +0530

    changes

---
+++ gateway/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const proxy = require('express-http-proxy');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/user', proxy('http://localhost:8001'))
+app.use('/tasks', proxy('http://localhost:8002'))
+app.use('/', proxy('http://localhost:8001'))
+
+
+app.listen(8000, () => {
+    console.log('Gateway is Listening to Port 8000')
+})
